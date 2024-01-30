a7b6ad02952f486ee03196240c94c7b6946122d6
Louis-Philippe Viens <2034421@cegepmontpetit.ca>
2024-01-30 14:32:02 -0500

    Labo 2 semaine 2 cours 1 completer

--- 4204W6_labo2/labo2/src/app/app.component.ts
+++ 4204W6_labo2/labo2/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,22 +12,32 @@ export class AppComponent {
   // ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
   // Une variable devra être ajoutée ici
   // ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+  similarArtists : string[] = [];
   result : boolean = false;
   artist : string = "";
 
   // ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
   // Le constructeur devra être ajouté ici
   // ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+  constructor(public http : HttpClient){}
 
   searchArtist():void{
     this.result = true;
-	
+  }
     // ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 	  // La requête HTTP devra être ajoutée ici
     // ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-
+  async request() : Promise<void>{
+    //https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=cher&api_key=YOUR_API_KEY&format=json
+    let x = await lastValueFrom(this.http.get<any>("https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+this.artist+"&api_key=e34ebf8561ba7c653a21d1d99a1a0070&format=json"));
+    console.log(x);
+    this.similarArtists = [];
+    for(let artist of x.similarartists.artist){
+      this.similarArtists.push(artist.name);
+    }
+    
+    console.log(this.similarArtists);
   }
-
   newSearch():void{
     this.result = false;
   }
